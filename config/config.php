30c8ab7c41c9d65af0ab25e6e7f5e32965bc7a85
mahesh <mahesh@qtechsoftware.com>
2018-03-09 18:15:37 +0530

    changes on noti

--- config/config.php
+++ config/config.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
 //Note: This file should be include first in every php page.
 
 define('BASE_PATH', dirname(dirname(__FILE__)));
-define('APP_FOLDER','simpleadmin');
+define('APP_FOLDER', 'simpleadmin');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 define("SERVER_KEY", "AAAAf5yEtTk:APA91bFxWSrFVMxPLVwFs98M7EDZ1pf9D5Lu5P6OXXSCIS1IVgZH05T9wuDqRwJ_O9hoy3n46oEFJGY1dN0YJ1Erl4zke-1B6UbssAXC4wgQ0rPXza5OtNUiv08aDSniaHIARvMMErsD");
 $firebase_path = "https://fcm.googleapis.com/fcm/send";
 
-require_once BASE_PATH.'/lib/MysqliDb.php';
+require_once BASE_PATH . '/lib/MysqliDb.php';
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "corephpadmin";
 // create connection object
-$db = new MysqliDb($servername,$username,$password,$dbname);
+$db = new MysqliDb($servername, $username, $password, $dbname);
 
 //default functions for some operations
 function random_string($length) {
@@ -29,10 +29,11 @@ function random_string($length) {
 }
 
 function sendPushNotificationToGCMSever($token, $message) {
+    $msgSize = sizeof($message);
     $fields = array(
-        'to' => $token,
-        'notification' => array('title' => 'FFS Notification', 'body' => $message),
-        //'data' => array('message' => $message)
+        'registration_ids' => $token,
+        'notification' => array('title' => 'FFS Notification', 'body' => 'Yo have '. $msgSize.' pending task'),
+        'data' => array('message' => $message)
     );
     $headers = array(
         'Authorization:key=' . SERVER_KEY,
@@ -43,6 +44,7 @@ function sendPushNotificationToGCMSever($token, $message) {
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
@@ -53,4 +55,5 @@ function sendPushNotificationToGCMSever($token, $message) {
 
     return $result;
 }
+
 ?>
--- sentNotification.php
+++ sentNotification.php
@@ -1,4 +1,5 @@
 <?php
+
 //https://inducesmile.com/android/android-firebase-cloud-messaging-push-notification-with-server-admin-in-php-and-mysql-database/
 session_start();
 require_once './config/config.php';
@@ -14,28 +15,28 @@ foreach ($result_task as $value_task) {
     //get device Id
     $query_deviceId = "Select deviceId from admin_accounts where id = '" . $userId . "'";
     $result_device = $db->query($query_deviceId);
+
     foreach ($result_device as $valueDevice) {
         $token = $valueDevice['deviceId'];
         $msg = "Your task is open for taskId " . $taskId . " <br/> Task Infor " . $taskInfo;
         $message = array("FCM" => $msg);
         array_push($tokenArray, $token);
         array_push($messageArray, $message);
-          $jsonString = sendPushNotificationToGCMSever($tokenArray, $messageArray);
-            $jsonObject = json_decode($jsonString);
     }
 }
 
-if (count($tokenArray) > 0) {
-    
+
+if (count($result_task) > 0) {
+
     $jsonString = sendPushNotificationToGCMSever($tokenArray, $messageArray);
     $jsonObject = json_decode($jsonString);
 //echo "userId ".$userId." TaskId ".$taskId." taskInfo ".$taskInfo;
-    echo "<br/> token " . $token . "<br/><br/>";
-  //  echo '$jsonString := ' . $jsonString . '<br/><br/>';
-
-
+    //  echo "<br/> jsonString " . $jsonString . "<br/><br/>";
+    //  echo '$jsonString := ' . $jsonString . '<br/><br/>';
+    //echo 'jobject '.$jsonObject;
     print_r($jsonObject);
 }
-header('location: index.php');
+
+//header('location: index.php');
 ?>
 
